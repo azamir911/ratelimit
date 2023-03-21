178e90ce2cb74837baeb698d9e2cfd044a7106d3
Avishay <zamir80@gmail.com>
2023-03-21 14:29:15 +0200

    Added mutexes map for lock each element

--- rate_limit/rate_limit.go
+++ rate_limit/rate_limit.go
@@ -4,21 +4,19 @@ import (
 	"errors"
 	"github.com/google/uuid"
 	"golang.org/x/net/context"
+	"log"
 	"net/url"
 	"sync"
 	"time"
 )
 
 // tickerDuration declared the duration to run the ticker with
-var tickerDuration = 5 * time.Minute
-
-//var tickerDuration = 10 * time.Second
+var tickerDuration = time.Hour
 
 func newRateLimiter(threshold int, ttl int) RateLimiter {
 	ctx := context.WithValue(context.Background(), "RateLimit", "True")
-	r := &rateLimiterCounter{
-		counter:   make(map[uuid.UUID]*rateLimitValue),
-		mut:       sync.RWMutex{},
+	r := &rateLimiter{
+		elements:  make(map[uuid.UUID]*value),
 		threshold: threshold,
 		ttl:       ttl,
 		ctx:       ctx,
@@ -32,10 +30,11 @@ func newRateLimiter(threshold int, ttl int) RateLimiter {
 	return r
 }
 
-type rateLimiterCounter struct {
-	// counter is a map that holds all the requests its attributes
-	counter map[uuid.UUID]*rateLimitValue
-	mut     sync.RWMutex
+type rateLimiter struct {
+	// elements is a map that holds all the requests its attributes
+	elements map[uuid.UUID]*value
+	mutexes  sync.Map
+	//mut     sync.RWMutex
 	// threshold from arguments
 	threshold int
 	// TTL from arguments
@@ -45,24 +44,25 @@ type rateLimiterCounter struct {
 	ctx context.Context
 }
 
-type rateLimitValue struct {
+type value struct {
 	ttl     time.Time
 	counter int
 }
 
 // runTicker should run in a different goroutine and responsible to clean the rate limit
-func runTicker(r *rateLimiterCounter) {
+func runTicker(r *rateLimiter) {
 	for {
 		select {
 		case <-r.ticker.C:
 			r.clean()
 		case <-r.ctx.Done():
+			log.Printf("Ticker context was done")
 			return
 		}
 	}
 }
 
-func (r *rateLimiterCounter) Allow(url *url.URL) (int, bool, error) {
+func (r *rateLimiter) Allow(url *url.URL) (int, bool, error) {
 	if url == nil {
 		return 0, false, errors.New("nil url")
 	}
@@ -72,51 +72,74 @@ func (r *rateLimiterCounter) Allow(url *url.URL) (int, bool, error) {
 		return 0, false, err
 	}
 
-	r.mut.Lock()
-	defer r.mut.Unlock()
+	// Getting a lock for an element
+	unlock := r.lock(key)
+	// Making sure to release the lock (can be written after the if statement instead, line #94)
+	defer unlock()
 
-	value, ok := r.counter[key]
+	v, ok := r.elements[key]
 	utcTime := time.Now().UTC()
-	if !ok || r.isReachTtl(*value, utcTime) {
-		// In case it's the first time for URL, or it already reaches the TTL, a new rateLimitValue will created
-		value = &rateLimitValue{
+	if !ok || r.isReachTtl(*v, utcTime) {
+		// In case it's the first time for URL, or it already reaches the TTL, a new value will create
+		v = &value{
 			ttl:     utcTime.Add(time.Millisecond * time.Duration(r.ttl)),
 			counter: 1,
 		}
-		r.counter[key] = value
+		r.elements[key] = v
 	} else {
 		// Increasing the counter
-		value.counter++
+		v.counter++
 	}
 
 	// Validate if the URL is reach the threshold
-	isReachCounter := r.isCounterReachThreshold(*value)
+	isReachCounter := r.isCounterReachThreshold(*v)
 
-	return value.counter, !isReachCounter, nil
+	return v.counter, !isReachCounter, nil
 }
 
-func (r *rateLimiterCounter) Stop() {
+func (r *rateLimiter) Stop() {
 	r.ctx.Done()
 }
 
+func (r *rateLimiter) lock(key uuid.UUID) func() {
+	mutValue, _ := r.mutexes.LoadOrStore(key, &sync.Mutex{})
+	mut := mutValue.(*sync.Mutex)
+	mut.Lock()
+	return func() {
+		mut.Unlock()
+	}
+}
+
 // isReachTtl return true in case the TTL has passed
-func (r *rateLimiterCounter) isReachTtl(value rateLimitValue, utcTime time.Time) bool {
+func (r *rateLimiter) isReachTtl(value value, utcTime time.Time) bool {
 	return utcTime.After(value.ttl)
 }
 
-// isCounterReachThreshold return true in case the counter reach the threshold
-func (r *rateLimiterCounter) isCounterReachThreshold(value rateLimitValue) bool {
+// isCounterReachThreshold return true in case the elements reach the threshold
+func (r *rateLimiter) isCounterReachThreshold(value value) bool {
 	return value.counter > r.threshold
 }
 
-// clean removing old elements from the counter map. It makes sure to clean unused URLs
-func (r *rateLimiterCounter) clean() {
+// clean removing old elements from the elements map. It makes sure to clean unused URLs
+func (r *rateLimiter) clean() {
+	log.Printf("Staring clean")
 	utcTime := time.Now().UTC()
-	for key, value := range r.counter {
-		if r.isReachTtl(*value, utcTime) {
-			r.mut.Lock()
-			delete(r.counter, key)
-			r.mut.Unlock()
+	for key, v := range r.elements {
+		if r.isReachTtl(*v, utcTime) {
+			unlock := r.lock(key)
+
+			// Do double check in case something was change till we got a lock for the element
+			v2 := r.elements[key]
+			if r.isReachTtl(*v2, utcTime) {
+				// Delete from elements
+				delete(r.elements, key)
+				// Delete from mutexes too
+				r.mutexes.Delete(key)
+				log.Printf("Key '%s' was released", key)
+			}
+
+			unlock()
 		}
 	}
+	log.Printf("Clean was successfully finished")
 }
